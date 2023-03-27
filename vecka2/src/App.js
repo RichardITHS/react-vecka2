29a5f23c90bf4d07a7ef4202b6a5a65c72b8b9f9
Navigoo <navigoo@me.com>
2023-03-27 09:51:49 +0200

    update

---
+++ vecka2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+//Importer av komponter
+import ConditionalRendering from './components/ConditionalRendering'
+
+function App() {
+    return (
+        <div className="App">
+            <ConditionalRendering />
+            
+        </div>
+    )
+}
+
+export default App
